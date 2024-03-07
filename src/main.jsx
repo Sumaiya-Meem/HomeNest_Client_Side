af4b2765fe12b1b61f1e39b3ed042dc39a433f3d
Sumaiya-Meem <sumaiyameem.cse6.bu@gmail.com>
2024-03-07 12:40:43 +0600

    add react helmet

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,19 @@
-
 import ReactDOM from "react-dom/client";
-import React from 'react'
+import React from "react";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import router from "./Routes/Routes.jsx";
 import AuthProvider from "./Context/AuthProvider.jsx";
+import { HelmetProvider } from "react-helmet-async";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
- <AuthProvider>
- <div className="">
-    <RouterProvider router={router} />
-  </div>
-  </AuthProvider>
+    <AuthProvider>
+      <HelmetProvider>
+        <div className="">
+          <RouterProvider router={router} />
+        </div>
+      </HelmetProvider>
+    </AuthProvider>
   </React.StrictMode>
-  
- 
 );
