ccd6028fc38a0c8800f7aa5f476dc7922591a1fc
Sumaiya-Meem <sumaiyameem.cse6.bu@gmail.com>
2024-03-14 17:00:38 +0600

    make search bar responsive for all device

--- src/Pages/DashboardPage/Dashboard/Dashboard.jsx
+++ src/Pages/DashboardPage/Dashboard/Dashboard.jsx
@@ -7,8 +7,8 @@ import { AuthContext } from "../../../Context/AuthProvider";
 import useAxiosSecure from "../../../hooks/useAxiosSecure";
 import { NavLink } from "react-router-dom";
 import logo from "../../../../public/icon.png";
-import {  Avatar, Dropdown, Navbar, TextInput } from "flowbite-react";
-import { MdSearch } from "react-icons/md";
+import { Avatar, Dropdown, Navbar, TextInput } from "flowbite-react";
+import { FaSearch } from "react-icons/fa";
 import { IoMdNotificationsOutline } from "react-icons/io";
 import { MdMessage } from "react-icons/md";
 
@@ -17,6 +17,7 @@ const Dashboard = () => {
   const axiosSecure = useAxiosSecure();
   const [isAgent, setIsAgent] = useState(false);
   const [isUser, setIsUser] = useState(false);
+  const [isSearch, setIsSearch] = useState(false);
 
   const [navCollpase, setNavCollpase] = useState(false);
   const [smallnavCollpase, setSmallNavCollpase] = useState(false);
@@ -44,66 +45,94 @@ const Dashboard = () => {
     <div>
       {/* header  start*/}
       <div className="flex mb-1 items-center justify-between p-2">
-        {/* logo content div */}
         <div className="flex items-center">
-          <img src={logo} className="mr-3 h-6 sm:h-9 ml-3" alt="Logo" />
-          <span className="self-center whitespace-nowrap text-xl font-bold text-[#000] italic">
-            <span className="text-[#bc0024]">H</span>ome
-            <span className="text-[#bc0024]">N</span>est
-          </span>
+          {/* logo content div */}
+          <div className="flex items-center">
+            <img src={logo} className="mr-3 h-6 sm:h-9 ml-3" alt="Logo" />
+            <span className="self-center whitespace-nowrap text-xl font-bold text-[#000] italic">
+              <span className="text-[#bc0024]">H</span>ome
+              <span className="text-[#bc0024]">N</span>est
+            </span>
+          </div>
+          <div className="">
+            <TfiAlignJustify
+              className="ml-5 md:ml-3 lg:ml-10 text-2xl largeDevice_justify"
+              onClick={() => setNavCollpase(!navCollpase)}
+            ></TfiAlignJustify>
+            <TfiAlignJustify
+              className="ml-5 md:ml-3 lg:ml-10 text-2xl smallDevice_justify"
+              onClick={() => setSmallNavCollpase(!smallnavCollpase)}
+            ></TfiAlignJustify>
+          </div>
         </div>
-        {/* justify icon div */}
-        <div className="">
-          <TfiAlignJustify
-            className="ml-10 text-2xl largeDevice_justify"
-            onClick={() => setNavCollpase(!navCollpase)}
-          ></TfiAlignJustify>
-          <TfiAlignJustify
-            className="ml-10 text-2xl smallDevice_justify"
-            onClick={() => setSmallNavCollpase(!smallnavCollpase)}
-          ></TfiAlignJustify>
-        </div>
+
         {/* search div */}
-        <div className="max-w-md">
-          <TextInput
-            type="text"
-           
-            placeholder="search"
-            required
-          />
+        <div className="">
+          <div className="relative md:w-96 flex items-center">
+            <input
+              type="search" name=""  placeholder="Search here"
+              className="hidden md:block  md:w-full px-4 py-1  rounded shadow-md outline-none"
+            />
+            <span className="absolute right-2">
+              <button className="md:hidden focus:outline-none text-black" onClick={()=>setIsSearch(!isSearch)}>
+                <FaSearch></FaSearch>
+              </button>
+            </span>
+          </div>
         </div>
-        
+
         <div className="flex items-center gap-2">
-            <IoMdNotificationsOutline className="text-2xl"></IoMdNotificationsOutline>
-            <MdMessage className="text-[21px]"></MdMessage>
+          <IoMdNotificationsOutline className="text-2xl"></IoMdNotificationsOutline>
+          <MdMessage className="text-[21px]"></MdMessage>
 
-            <Navbar >
-      
-      <div className="flex">
-        <Dropdown
-          arrowIcon={false}
-          inline
-          label={
-            <Avatar alt="User settings" img="https://flowbite.com/docs/images/people/profile-picture-5.jpg" rounded />
-          }
-        >
-          <Dropdown.Header>
-            <span className="block text-sm">Bonnie Green</span>
-            <span className="block truncate text-sm font-medium">name@flowbite.com</span>
-          </Dropdown.Header>
-          <Dropdown.Item>Dashboard</Dropdown.Item>
-          <Dropdown.Item>Settings</Dropdown.Item>
-          <Dropdown.Item>Earnings</Dropdown.Item>
-          <Dropdown.Divider />
-          <Dropdown.Item>Sign out</Dropdown.Item>
-        </Dropdown>
+          <Navbar>
+            <div className="flex">
+              <Dropdown
+                arrowIcon={false}
+                inline
+                label={
+                  <Avatar
+                    alt="User settings"
+                    img="https://flowbite.com/docs/images/people/profile-picture-5.jpg"
+                    rounded
+                  />
+                }
+              >
+                <Dropdown.Header>
+                  <span className="block text-sm">Bonnie Green</span>
+                  <span className="block truncate text-sm font-medium">
+                    name@flowbite.com
+                  </span>
+                </Dropdown.Header>
+                <Dropdown.Item>Dashboard</Dropdown.Item>
+                <Dropdown.Item>Settings</Dropdown.Item>
+                <Dropdown.Item>Earnings</Dropdown.Item>
+                <Dropdown.Divider />
+                <Dropdown.Item>Sign out</Dropdown.Item>
+              </Dropdown>
+            </div>
+          </Navbar>
         </div>
-        </Navbar>
-       
-
-      </div>  {/* header end */}
-      
-      </div> 
+      </div>
+      {/* search for small device */}
+      {
+        isSearch ? <>
+        <div className="sm-search">
+          <div className="relative md:w-56 flex items-center mx-2">
+            <input
+              type="search" name=""  placeholder="Search here"
+              className="w-full px-4 py-1  rounded shadow-sm outline-none"
+            />
+            <span className="absolute right-2">
+              <button className=" focus:outline-none text-black">
+                <FaSearch></FaSearch>
+              </button>
+            </span>
+          </div>
+        </div>
+        </> : " "
+      }
+      {/* header end */}
       <div className="md:flex">
         <div
           className={`${
